56839e2b5b0e988e7d6344bc5ea095b7bed0c8a3
dev.Wladimir <50156678+dev2wl@users.noreply.github.com>
2024-09-01 02:45:51 +0300

    Added installation: zsh plugin zinit hombrew

---
+++ app/homebrew/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check if Homebrew is already installed
+if ! command -v brew &> /dev/null
+then
+  echo "Homebrew not found. Installing..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Homebrew is already installed."
+fi
+
---
+++ app/zsh/zsh/aliases.zsh
@@ -0,0 +1,8 @@
+# Check my IP
+alias myip="curl http://ipecho.net/plain; echo"
+
+# Detailed list of directory
+alias l="ls -lAFh"
+
+# Sourcing zshrc files
+alias reset="command reset && source ~/.zshrc"
